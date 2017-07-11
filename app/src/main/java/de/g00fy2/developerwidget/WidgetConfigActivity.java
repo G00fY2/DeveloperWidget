33ce47da80dcfc882664150406e3cb67bafb096e
Thomas Wirth <thomas-w-@web.de>
2017-07-12 01:17:28 +0200

    Add testing method to get versionName and versionCode of apps

--- app/src/main/java/de/g00fy2/developerwidget/WidgetConfigActivity.java
+++ app/src/main/java/de/g00fy2/developerwidget/WidgetConfigActivity.java
@@ -3,6 +3,8 @@ package de.g00fy2.developerwidget;
 import android.appwidget.AppWidgetManager;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -55,6 +57,7 @@ public class WidgetConfigActivity extends AppCompatActivity {
     remoteViews.setTextViewText(R.id.device_info, getDeviceInfo());
     remoteViews.setTextViewText(R.id.release, getVersionAndCodename());
     remoteViews.setTextViewText(R.id.sdk_int, getSDKVersion());
+    remoteViews.setTextViewText(R.id.app_info, getAppInfo());
   }
 
   private String getDeviceInfo() {
@@ -72,5 +75,20 @@ public class WidgetConfigActivity extends AppCompatActivity {
 
   private String getSDKVersion() {
     return "API " + Integer.toString(Build.VERSION.SDK_INT);
+  }
+
+  private String getAppInfo() {
+    PackageInfo pInfo;
+    String version = "";
+    int verCode = 0;
+    try {
+      pInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
+      version = pInfo.versionName;
+      verCode = pInfo.versionCode;
+    } catch (PackageManager.NameNotFoundException e) {
+      e.printStackTrace();
+    }
+
+    return version + " (" + Integer.toString(verCode) + ")";
   }
 }
