58896ebb5731e81fd2473054a8b37d36f8dfc30b
G00fY2 <thomas-w-@web.de>
2018-10-05 00:40:29 +0200

    Add new about items with icon and string resources to the about page

--- app/src/main/kotlin/de/g00fy2/developerwidget/about/AboutActivity.kt
+++ app/src/main/kotlin/de/g00fy2/developerwidget/about/AboutActivity.kt
@@ -3,20 +3,25 @@ package de.g00fy2.developerwidget.about
 import android.R.id
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.squareup.picasso3.Picasso
 import com.squareup.picasso3.Picasso.Builder
 import de.g00fy2.developerwidget.BuildConfig
 import de.g00fy2.developerwidget.R
 import de.g00fy2.developerwidget.R.layout
-import de.g00fy2.developerwidget.util.CircleTransformation
 import de.g00fy2.developerwidget.util.SharedPreferencesHelper
 import de.g00fy2.developerwidget.web.GithubAPI
 import de.g00fy2.developerwidget.web.model.Release
 import de.g00fy2.developerwidget.web.model.Repository
 import kotlinx.android.synthetic.main.activity_about.app_desc_textview
 import kotlinx.android.synthetic.main.activity_about.app_version_textview
-import kotlinx.android.synthetic.main.activity_about.author_imageview
+import kotlinx.android.synthetic.main.activity_about.author_header
+import kotlinx.android.synthetic.main.activity_about.build_number
+import kotlinx.android.synthetic.main.activity_about.changelog_item
+import kotlinx.android.synthetic.main.activity_about.license_item
+import kotlinx.android.synthetic.main.activity_about.open_source_licenses
+import kotlinx.android.synthetic.main.activity_about.source_code_item
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.IO
@@ -54,8 +59,14 @@ class AboutActivity : AppCompatActivity(), CoroutineScope {
     app_version_textview.text =
         String.format(getString(R.string.app_version), BuildConfig.VERSION_NAME, BuildConfig.VERSION_CODE)
     app_desc_textview.text = sharedPreferencesHelper.getString(SharedPreferencesHelper.GITHUB_PROJECT_DESC) ?: ""
-    picasso.load(sharedPreferencesHelper.getString(SharedPreferencesHelper.GITHUB_AUTHOR_IMAGE_URL))
-      .transform(CircleTransformation()).into(author_imageview)
+
+    license_item.setIcon(R.drawable.ic_open_source_logo).setTitle(R.string.license).setDescription(R.string.mit_license)
+    source_code_item.setIcon(R.drawable.ic_github_logo_shape).setTitle(R.string.source_code)
+    changelog_item.setIcon(R.drawable.ic_changes_logo).setTitle(R.string.changelog)
+    open_source_licenses.setIcon(R.drawable.ic_open_source_logo).setTitle(R.string.open_source_licenses)
+    build_number.setIcon(R.drawable.ic_about_logo).setTitle(R.string.build_number)
+      .setDescription(BuildConfig.VERSION_NAME + "." + BuildConfig.VERSION_CODE + "." + BuildConfig.BUILD_TYPE)
+    (author_header as TextView).setText(R.string.author)
   }
 
   override fun onDestroy() {
@@ -105,7 +116,6 @@ class AboutActivity : AppCompatActivity(), CoroutineScope {
           releaseInfo?.author?.avatarUrl
         )
         app_desc_textview.text = repositoryInfo?.description ?: ""
-        picasso.load(releaseInfo?.author?.avatarUrl).transform(CircleTransformation()).into(author_imageview)
       } catch (e: IOException) {
         Timber.d(e)
       }
