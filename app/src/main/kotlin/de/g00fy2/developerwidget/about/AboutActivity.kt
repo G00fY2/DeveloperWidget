ff91e27d535dcbea066ac87c2c40a05fe3c8714b
G00fY2 <thomas-w-@web.de>
2018-10-07 16:18:52 +0200

    Remove github api connection and network logic

--- app/src/main/kotlin/de/g00fy2/developerwidget/about/AboutActivity.kt
+++ app/src/main/kotlin/de/g00fy2/developerwidget/about/AboutActivity.kt
@@ -4,16 +4,11 @@ import android.R.id
 import android.os.Bundle
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import com.squareup.picasso3.Picasso
-import com.squareup.picasso3.Picasso.Builder
 import de.g00fy2.developerwidget.BuildConfig
 import de.g00fy2.developerwidget.R
 import de.g00fy2.developerwidget.R.layout
 import de.g00fy2.developerwidget.util.Constants
 import de.g00fy2.developerwidget.util.SharedPreferencesHelper
-import de.g00fy2.developerwidget.web.GithubAPI
-import de.g00fy2.developerwidget.web.model.Release
-import de.g00fy2.developerwidget.web.model.Repository
 import kotlinx.android.synthetic.main.activity_about.app_desc_textview
 import kotlinx.android.synthetic.main.activity_about.app_version_textview
 import kotlinx.android.synthetic.main.activity_about.author_header
@@ -26,37 +21,18 @@ import kotlinx.android.synthetic.main.activity_about.open_source_licenses_item
 import kotlinx.android.synthetic.main.activity_about.privacy_item
 import kotlinx.android.synthetic.main.activity_about.source_code_item
 import kotlinx.android.synthetic.main.activity_about.twitter_item
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.IO
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.android.Main
-import kotlinx.coroutines.async
-import kotlinx.coroutines.launch
-import okhttp3.Cache
-import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
-import timber.log.Timber
-import java.io.IOException
-import kotlin.coroutines.CoroutineContext
 
-class AboutActivity : AppCompatActivity(), CoroutineScope {
+class AboutActivity : AppCompatActivity() {
 
-  private lateinit var job: Job
-  private lateinit var okHttpClient: OkHttpClient
-  private lateinit var picasso: Picasso
-  private lateinit var githubAPI: GithubAPI
   private lateinit var sharedPreferencesHelper: SharedPreferencesHelper
 
   public override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    job = Job()
     setContentView(layout.activity_about)
     supportActionBar?.setDisplayHomeAsUpEnabled(true)
     supportActionBar?.elevation = 0f
-    setupDependencies()
+    sharedPreferencesHelper = SharedPreferencesHelper(this)
     initView()
-    getGithubInformation()
   }
 
   private fun initView() {
@@ -82,56 +58,12 @@ class AboutActivity : AppCompatActivity(), CoroutineScope {
       .enableHonorClicking(true)
   }
 
-  override fun onDestroy() {
-    super.onDestroy()
-    job.cancel()
-  }
-
-  override val coroutineContext: CoroutineContext
-    get() = Dispatchers.Main + job
-
   override fun onOptionsItemSelected(item: MenuItem?): Boolean {
     return if (item?.itemId == id.home) {
       finish()
       true
     } else {
       super.onOptionsItemSelected(item)
-    }
-  }
-
-  private fun setupDependencies() {
-    sharedPreferencesHelper = SharedPreferencesHelper(this)
-    okHttpClient = OkHttpClient.Builder().cache(Cache(this.cacheDir, 10L * 1024 * 1024))
-      .addNetworkInterceptor(
-        HttpLoggingInterceptor(HttpLoggingInterceptor.Logger { Timber.d(it) }).setLevel(
-          HttpLoggingInterceptor.Level.BODY
-        )
-      ).build()
-    picasso = Builder(this).client(okHttpClient).indicatorsEnabled(BuildConfig.DEBUG).build()
-    githubAPI = GithubAPI(okHttpClient)
-  }
-
-  private fun getGithubInformation() {
-    launch {
-      val releaseInfo: Release?
-      val repositoryInfo: Repository?
-      try {
-        releaseInfo = async(Dispatchers.IO) {
-          githubAPI.getGithubReleaseInfo().await()
-        }.await()
-        repositoryInfo = async(Dispatchers.IO) {
-          githubAPI.getGithubRepositoryInfo().await()
-        }.await()
-
-        sharedPreferencesHelper.putString(SharedPreferencesHelper.GITHUB_PROJECT_DESC, repositoryInfo?.description)
-        sharedPreferencesHelper.putString(
-          SharedPreferencesHelper.GITHUB_AUTHOR_IMAGE_URL,
-          releaseInfo?.author?.avatarUrl
-        )
-        app_desc_textview.text = repositoryInfo?.description ?: ""
-      } catch (e: IOException) {
-        Timber.d(e)
-      }
     }
   }
 }
