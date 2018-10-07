111884a5954465b8b30bfe69b8650be0ce8fe853
G00fY2 <thomas-w-@web.de>
2018-10-07 15:12:35 +0200

    Add custom header item layout

---
+++ app/src/main/kotlin/de/g00fy2/developerwidget/about/views/AboutHeaderLayout.kt
@@ -0,0 +1,24 @@
+package de.g00fy2.developerwidget.about.views
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.widget.FrameLayout
+import androidx.annotation.StringRes
+import de.g00fy2.developerwidget.R.layout
+import kotlinx.android.synthetic.main.about_item_header.view.header_textview
+
+class AboutHeaderLayout : FrameLayout {
+
+  constructor(context: Context) : this(context, null)
+  constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0)
+  constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+    LayoutInflater.from(context).inflate(layout.about_item_header, this, true)
+  }
+
+  fun setTitle(@StringRes titleRes: Int): FrameLayout {
+    header_textview.setText(titleRes)
+    return this
+  }
+
+}
