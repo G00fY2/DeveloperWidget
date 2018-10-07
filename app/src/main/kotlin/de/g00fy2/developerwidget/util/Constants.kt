7d1d6590351bbd8996550c4eae5f67d768bc9412
G00fY2 <thomas-w-@web.de>
2018-10-07 19:44:07 +0200

    Remove obsolete todo

--- app/src/main/kotlin/de/g00fy2/developerwidget/util/Constants.kt
+++ app/src/main/kotlin/de/g00fy2/developerwidget/util/Constants.kt
@@ -3,7 +3,6 @@ package de.g00fy2.developerwidget.util
 class Constants {
 
   companion object {
-    //TODO add real github urls
     const val PRIVACY_POLICY = "https://g00fy2.github.io/DeveloperWidget/privacy-policy.html"
     const val ICON_CREDITS = "https://g00fy2.github.io/DeveloperWidget/icon-credits.html"
     const val GITHUB_PROJECT = "https://github.com/G00fY2/DeveloperWidget"
