236ee5996368e9da769ab6075aa4a40b67867a18
Thomas Wirth <thomas-w-@web.de>
2018-11-16 11:38:34 +0100

    Optimize cpu info logic

--- app/src/main/kotlin/de/g00fy2/developerwidget/data/cpu/CPUDataProvider.kt
+++ app/src/main/kotlin/de/g00fy2/developerwidget/data/cpu/CPUDataProvider.kt
@@ -4,12 +4,15 @@ import android.os.Build
 import android.os.Build.VERSION
 import android.os.Build.VERSION_CODES
 import java.io.File
+import java.io.FileNotFoundException
 import java.io.RandomAccessFile
 import java.util.regex.Pattern
 
 class CPUDataProvider {
 
   companion object {
+
+    private const val CPU_SYS_FOLDER = "/sys/devices/system/cpu/"
 
     fun getPrimaryABI(): String {
       return if (VERSION.SDK_INT >= VERSION_CODES.LOLLIPOP) {
@@ -23,28 +26,42 @@ class CPUDataProvider {
     fun getCPUCoreNum(): Int {
       val pattern = Pattern.compile("cpu[0-9]+")
       return Math.max(
-        File("/sys/devices/system/cpu/")
+        File(CPU_SYS_FOLDER)
           .walk()
+          .maxDepth(1)
           .count { pattern.matcher(it.name).matches() },
         Runtime.getRuntime().availableProcessors()
       )
     }
 
     fun getGroupedCPUCoreFrequencies(): String {
-      val pattern = Pattern.compile("cpu[0-9]+/cpufreq/(cpuinfo_max_freq|cpuinfo_min_freq)")
-      return File("/sys/devices/system/cpu/")
-        .walk()
-        .sorted()
-        .filter { pattern.matcher(it.path).find() }
-        .map { RandomAccessFile(it.path, "r").use { reader -> (reader.readLine().toLong() / 1000).toInt() } }
-        .windowed(2, 2)
-        .map { Pair(it[0], it[1]) }
-        .toList()
-        .sortedByDescending { it.first }
-        .groupingBy { it.first.toString() + "MHz - " + it.second + "MHz" }
+      val frequencies: MutableList<Pair<Int, Int>> = ArrayList()
+      val cores = getCPUCoreNum()
+      for (i in 0 until cores) {
+        getCPUFrequenciesPerCore(i)?.let { frequencies.add(it) }
+      }
+
+      return frequencies.sortedByDescending { it.second }
+        .groupingBy { it.second.toString() + "MHz - " + it.first + "MHz" }
         .eachCount()
         .map { it.value.toString() + " x " + it.key }
+        .plus(if (frequencies.size < cores) (cores - frequencies.size).toString() + " x offline" else "")
+        .filter { !it.isBlank() }
         .joinToString(separator = "\n")
+    }
+
+    private fun getCPUFrequenciesPerCore(core: Int): Pair<Int, Int>? {
+      return try {
+        val max = RandomAccessFile(CPU_SYS_FOLDER + "cpu$core/cpufreq/cpuinfo_max_freq", "r").use { reader ->
+          (reader.readLine().toLong() / 1000).toInt()
+        }
+        val min = RandomAccessFile(CPU_SYS_FOLDER + "cpu$core/cpufreq/cpuinfo_min_freq", "r").use { reader ->
+          (reader.readLine().toLong() / 1000).toInt()
+        }
+        Pair(min, max)
+      } catch (e: FileNotFoundException) {
+        null
+      }
     }
   }
 }
