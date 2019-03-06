6ab3de0c35fef456a8dde074b4fbd418b734818b
Thomas Wirth <thomas-w-@web.de>
2019-03-07 00:43:51 +0100

    Use AndroidInjectionModule instead AndroidSupportInjectionModule

--- app/src/main/kotlin/de/g00fy2/developerwidget/di/AppComponent.kt
+++ app/src/main/kotlin/de/g00fy2/developerwidget/di/AppComponent.kt
@@ -1,13 +1,13 @@
 package de.g00fy2.developerwidget.di
 
 import dagger.Component
+import dagger.android.AndroidInjectionModule
 import dagger.android.AndroidInjector
-import dagger.android.support.AndroidSupportInjectionModule
 import de.g00fy2.developerwidget.DevWidgetApp
 import javax.inject.Singleton
 
 @Singleton
-@Component(modules = [AndroidSupportInjectionModule::class, AppModule::class, ActivityBindingModule::class])
+@Component(modules = [AndroidInjectionModule::class, AppModule::class, ActivityBindingModule::class])
 interface AppComponent : AndroidInjector<DevWidgetApp> {
 
   @Component.Builder
