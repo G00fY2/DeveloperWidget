8ac831cfdeb948883910612811a4fffbf6a59eb7
Thomas Wirth <thomas-w-@web.de>
2019-06-06 21:55:59 +0200

    Optimize shortcut router logic and only enable the activity when shortcuts are supported

--- app/src/main/kotlin/com/g00fy2/developerwidget/activities/shortcut/ShortcutRouter.kt
+++ app/src/main/kotlin/com/g00fy2/developerwidget/activities/shortcut/ShortcutRouter.kt
@@ -2,22 +2,23 @@ package com.g00fy2.developerwidget.activities.shortcut
 
 import android.app.Activity
 import android.content.Intent
+import android.os.Bundle
 import android.widget.Toast
 import com.g00fy2.developerwidget.R
 
 class ShortcutRouter : Activity() {
 
-  override fun onResume() {
-    super.onResume()
-    when (intent.getStringExtra("shortcut_id")) {
-      "devsettings" -> startActivity(Intent(android.provider.Settings.ACTION_APPLICATION_DEVELOPMENT_SETTINGS).apply {
-        flags = Intent.FLAG_ACTIVITY_NEW_TASK
-      })
-      "langsettings" -> startActivity(Intent(android.provider.Settings.ACTION_LOCALE_SETTINGS).apply {
-        flags = Intent.FLAG_ACTIVITY_NEW_TASK
-      })
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+
+    when (intent.getStringExtra("extra_shortcut_id")) {
+      "devsettings" -> launchIntent(android.provider.Settings.ACTION_APPLICATION_DEVELOPMENT_SETTINGS)
+      "langsettings" -> launchIntent(android.provider.Settings.ACTION_LOCALE_SETTINGS)
       else -> Toast.makeText(this, getString(R.string.shortcut_error), Toast.LENGTH_SHORT).show()
     }
     finish()
   }
+
+  private fun launchIntent(action: String) =
+    startActivity(Intent(action).apply { flags = Intent.FLAG_ACTIVITY_NEW_TASK })
 }
