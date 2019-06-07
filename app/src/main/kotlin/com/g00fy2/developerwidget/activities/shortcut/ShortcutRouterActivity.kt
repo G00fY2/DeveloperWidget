a45375d7fcaaa03db9461a3268ca3b50422a5e51
Thomas Wirth <thomas-w-@web.de>
2019-06-07 13:02:12 +0200

    Rename shortcut router activity

--- app/src/main/kotlin/com/g00fy2/developerwidget/activities/shortcut/ShortcutRouterActivity.kt
+++ app/src/main/kotlin/com/g00fy2/developerwidget/activities/shortcut/ShortcutRouterActivity.kt
@@ -6,7 +6,7 @@ import android.os.Bundle
 import android.widget.Toast
 import com.g00fy2.developerwidget.R
 
-class ShortcutRouter : Activity() {
+class ShortcutRouterActivity : Activity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
