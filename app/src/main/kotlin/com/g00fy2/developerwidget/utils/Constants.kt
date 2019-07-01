8a59332a80e08ca71e613e81b7abfbfebbe36c5c
Thomas Wirth <thomas-w-@web.de>
2019-07-01 20:58:31 +0200

    Switch changelog url to github releases url

--- app/src/main/kotlin/com/g00fy2/developerwidget/utils/Constants.kt
+++ app/src/main/kotlin/com/g00fy2/developerwidget/utils/Constants.kt
@@ -10,8 +10,8 @@ const val DIALOG_ACTIVITY_WIDTH_FACTOR = 0.95
 const val DIALOG_ACTIVITY_HEIGHT_FACTOR = 0.80
 
 // Urls
-const val GITHUB_PROJECT = "https://github.com/G00fY2/DeveloperWidget/"
-const val CHANGES = "$GITHUB_PROJECT/blob/master/CHANGELOG.md"
+const val GITHUB_PROJECT = "https://github.com/G00fY2/DeveloperWidget"
+const val CHANGES = "$GITHUB_PROJECT/releases"
 const val MIT_LICENSE = "$GITHUB_PROJECT/blob/master/LICENSE"
 const val GITHUB_PROJECT_IO = "https://g00fy2.github.io/DeveloperWidget"
 const val PRIVACY_POLICY = "$GITHUB_PROJECT_IO/privacy-policy.html"
