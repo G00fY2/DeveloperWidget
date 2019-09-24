1c79915491452dafc238464fa746823f4670a8b9
Thomas Wirth <thomas-w-@web.de>
2019-09-24 21:01:54 +0200

    Use window constant for accessing the main layout container

--- app/src/main/kotlin/com/g00fy2/developerwidget/base/BaseActivity.kt
+++ app/src/main/kotlin/com/g00fy2/developerwidget/base/BaseActivity.kt
@@ -83,7 +83,7 @@ abstract class BaseActivity(private val isDialogActivity: Boolean = false) : App
             flags or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
         }
       }
-      findViewById<View>(android.R.id.content)?.let {
+      findViewById<View>(Window.ID_ANDROID_CONTENT)?.let {
         it.doOnApplyWindowInsets { view, insets, padding ->
           view.updatePadding(top = padding.top + insets.systemWindowInsetTop)
         }
