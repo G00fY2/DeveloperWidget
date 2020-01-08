6290886143246e48dd87603a210a0d0d4bf5b64b
Thomas Wirth <thomas-w-@web.de>
2020-01-08 14:50:48 +0100

    Use itemView to add ripple

--- app/src/main/kotlin/com/g00fy2/developerwidget/base/BaseViewHolder.kt
+++ app/src/main/kotlin/com/g00fy2/developerwidget/base/BaseViewHolder.kt
@@ -4,9 +4,9 @@ import androidx.recyclerview.widget.RecyclerView
 import androidx.viewbinding.ViewBinding
 import com.g00fy2.developerwidget.ktx.addRipple
 
-abstract class BaseViewHolder<T>(private val binding: ViewBinding) : RecyclerView.ViewHolder(binding.root) {
+abstract class BaseViewHolder<T>(binding: ViewBinding) : RecyclerView.ViewHolder(binding.root) {
 
   abstract fun onBind(item: T)
 
-  fun addRipple() = binding.root.addRipple(true)
+  fun addRipple() = itemView.addRipple(true)
 }
