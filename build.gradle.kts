a1bcd9ae5d0cc63609c2d9740335849f7c5c2397
Thomas Wirth <thomas-w-@web.de>
2020-02-06 01:27:36 +0100

    Remove license report plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,3 @@
-import com.github.jk1.license.filter.*
-import com.github.jk1.license.render.*
-
-plugins {
-  id("com.github.jk1.dependency-license-report") version "1.12"
-}
-
-licenseReport {
-  configurations = arrayOf("releaseRuntimeClasspath")
-  renderers = arrayOf<ReportRenderer>(SimpleHtmlReportRenderer(), JsonReportRenderer())
-  filters = arrayOf<DependencyFilter>(LicenseBundleNormalizer())
-}
-
 tasks.register("clean", Delete::class) {
   delete(rootProject.buildDir)
 }
