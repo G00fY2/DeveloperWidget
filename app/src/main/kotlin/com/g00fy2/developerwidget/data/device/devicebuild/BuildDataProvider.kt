30aee1d16743316fc2f40c1dc56f2ae2b07ff25c
Thomas Wirth <thomas-w-@web.de>
2020-02-24 19:37:05 +0100

    Use objects for data provider classes

--- app/src/main/kotlin/com/g00fy2/developerwidget/data/device/devicebuild/BuildDataProvider.kt
+++ app/src/main/kotlin/com/g00fy2/developerwidget/data/device/devicebuild/BuildDataProvider.kt
@@ -3,33 +3,30 @@ package com.g00fy2.developerwidget.data.device.devicebuild
 import android.annotation.SuppressLint
 import android.os.Build
 
-class BuildDataProvider {
+object BuildDataProvider {
 
-  companion object {
-
-    @SuppressLint("DefaultLocale")
-    fun getCombinedDeviceName(): String {
-      return if (Build.MODEL.contains(Build.MANUFACTURER, true)) {
-        Build.MODEL.capitalize()
-      } else {
-        Build.MANUFACTURER.capitalize() + " " + Build.MODEL.capitalize()
-      }
+  @SuppressLint("DefaultLocale")
+  fun getCombinedDeviceName(): String {
+    return if (Build.MODEL.contains(Build.MANUFACTURER, true)) {
+      Build.MODEL.capitalize()
+    } else {
+      Build.MANUFACTURER.capitalize() + " " + Build.MODEL.capitalize()
     }
-
-    fun getBoard(): String = Build.BOARD
-
-    fun getDevice(): String = Build.DEVICE
-
-    fun getHardware(): String = Build.HARDWARE
-
-    fun getProduct(): String = Build.PRODUCT
-
-    fun getModel(): String = Build.MODEL
-
-    fun getManufacture(): String = Build.MANUFACTURER
-
-    fun getBootloader(): String = Build.BOOTLOADER
-
-    fun getID(): String = Build.ID
   }
+
+  fun getBoard(): String = Build.BOARD
+
+  fun getDevice(): String = Build.DEVICE
+
+  fun getHardware(): String = Build.HARDWARE
+
+  fun getProduct(): String = Build.PRODUCT
+
+  fun getModel(): String = Build.MODEL
+
+  fun getManufacture(): String = Build.MANUFACTURER
+
+  fun getBootloader(): String = Build.BOOTLOADER
+
+  fun getID(): String = Build.ID
 }
--- app/src/main/kotlin/com/g00fy2/developerwidget/data/device/system/SystemDataProvider.kt
+++ app/src/main/kotlin/com/g00fy2/developerwidget/data/device/system/SystemDataProvider.kt
@@ -5,49 +5,46 @@ import android.os.Build.VERSION
 import android.os.Build.VERSION_CODES
 import com.g00fy2.versioncompare.Version
 
-class SystemDataProvider {
+object SystemDataProvider {
 
-  companion object {
+  fun getVersionAndSDK(): Pair<String, String> = Pair(getAndroidVersion(), getSDKLevel())
 
-    fun getVersionAndSDK(): Pair<String, String> = Pair(getAndroidVersion(), getSDKLevel())
+  fun getAndroidVersion(): String = VERSION.RELEASE
 
-    fun getAndroidVersion(): String = VERSION.RELEASE
+  fun getCodename(): String = VERSION.CODENAME
 
-    fun getCodename(): String = VERSION.CODENAME
-
-    @SuppressLint("DefaultLocale")
-    fun getSDKLevel(): String {
-      getCodename().let {
-        return if (it.isBlank() || it.equals("REL", true)) {
-          VERSION.SDK_INT.toString()
-        } else {
-          it.toUpperCase()
-        }
-      }
-    }
-
-    fun getPreviewSDK(): String {
-      return if (VERSION.SDK_INT >= VERSION_CODES.M) {
-        VERSION.PREVIEW_SDK_INT.toString()
+  @SuppressLint("DefaultLocale")
+  fun getSDKLevel(): String {
+    getCodename().let {
+      return if (it.isBlank() || it.equals("REL", true)) {
+        VERSION.SDK_INT.toString()
       } else {
-        ""
+        it.toUpperCase()
       }
     }
-
-    fun getPatchLevel(): String {
-      return if (VERSION.SDK_INT >= VERSION_CODES.M) {
-        VERSION.SECURITY_PATCH
-      } else {
-        ""
-      }
-    }
-
-    fun getVMVersion(): String {
-      val vmVersion = System.getProperty("java.vm.version")
-      val vmName = if (Version(vmVersion).isAtLeast("2", true)) "ART" else "Dalvik"
-      return "$vmName $vmVersion"
-    }
-
-    fun getKernelVersion(): String = System.getProperty("os.version") ?: ""
   }
+
+  fun getPreviewSDK(): String {
+    return if (VERSION.SDK_INT >= VERSION_CODES.M) {
+      VERSION.PREVIEW_SDK_INT.toString()
+    } else {
+      ""
+    }
+  }
+
+  fun getPatchLevel(): String {
+    return if (VERSION.SDK_INT >= VERSION_CODES.M) {
+      VERSION.SECURITY_PATCH
+    } else {
+      ""
+    }
+  }
+
+  fun getVMVersion(): String {
+    val vmVersion = System.getProperty("java.vm.version")
+    val vmName = if (Version(vmVersion).isAtLeast("2", true)) "ART" else "Dalvik"
+    return "$vmName $vmVersion"
+  }
+
+  fun getKernelVersion(): String = System.getProperty("os.version") ?: ""
 }
