4abb11362aa570d60a932977da4dcc2418657979
Thomas Wirth <thomas-w-@web.de>
2020-02-27 20:48:12 +0100

    Update android studio

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,7 +13,7 @@ pluginManagement {
   resolutionStrategy {
     eachPlugin {
       if (requested.id.namespace == "com.android") {
-        useModule("com.android.tools.build:gradle:4.0.0-beta01")
+        useModule("com.android.tools.build:gradle:4.1.0-alpha01")
       }
       if (requested.id.id == "eu.appcom.gradle.android-versioning") {
         useModule("eu.appcom.gradle:android-versioning:1.0.2")
