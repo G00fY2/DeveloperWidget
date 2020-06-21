7535ed93c6bcad57466ed4734c947cfb75049b1a
Thomas Wirth <thomas-w-@web.de>
2020-06-21 10:45:28 +0200

    Update android studio and versioning plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,23 +3,19 @@ include("app")
 pluginManagement {
   val kotlinVersion: String by settings
   val androidGradlePluginVersion: String by settings
+  val nanogiantsVersioning: String by settings
   val gradleEnterpriseVersion: String by settings
   repositories {
     google()
     gradlePluginPortal()
     mavenCentral()
-    jcenter {
-      content {
-        includeModule("eu.nanogiants", "android-versioning")
-      }
-    }
     maven ("https://dl.bintray.com/kotlin/kotlin-eap")
   }
   resolutionStrategy {
     eachPlugin {
       when (requested.id.id) {
         "com.android.application" -> useModule("com.android.tools.build:gradle:$androidGradlePluginVersion")
-        "de.nanogiants.android-versioning" -> useModule("de.nanogiants:android-versioning:2.3.1")
+        "de.nanogiants.android-versioning" -> useVersion(nanogiantsVersioning)
         "com.gradle.enterprise" -> useVersion(gradleEnterpriseVersion)
       }
       if (requested.id.namespace == "org.jetbrains.kotlin") useVersion(kotlinVersion)
