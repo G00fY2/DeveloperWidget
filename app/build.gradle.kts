e4f66d6cb906a16680cd982df1a18dc15a1a0cb2
Thomas Wirth <thomas-w-@web.de>
2020-06-25 17:47:10 +0200

    Update deps and gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -78,8 +78,8 @@ dependencies {
   implementation("androidx.core:core-ktx:1.5.0-alpha01")
   implementation("androidx.activity:activity:1.2.0-alpha06")
   implementation("androidx.fragment:fragment:1.3.0-alpha06")
-  implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.3.0-alpha04")
-  implementation("androidx.recyclerview:recyclerview:1.2.0-alpha03")
+  implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.3.0-alpha05")
+  implementation("androidx.recyclerview:recyclerview:1.2.0-alpha04")
   implementation("androidx.constraintlayout:constraintlayout:2.0.0-beta7")
   implementation("androidx.vectordrawable:vectordrawable:1.2.0-alpha01")
 
