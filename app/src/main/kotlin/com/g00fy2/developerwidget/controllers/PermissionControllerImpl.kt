440984b8348ee0a9409291a2f40c112a9e46cf4b
Thomas Wirth <thomas-w-@web.de>
2020-06-30 18:18:39 +0200

    Add API 30 all files access support

--- app/src/main/kotlin/com/g00fy2/developerwidget/controllers/PermissionControllerImpl.kt
+++ app/src/main/kotlin/com/g00fy2/developerwidget/controllers/PermissionControllerImpl.kt
@@ -1,6 +1,13 @@
 package com.g00fy2.developerwidget.controllers
 
+import android.Manifest
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build.VERSION
+import android.os.Build.VERSION_CODES
+import android.os.Environment
+import android.provider.Settings
 import androidx.activity.result.contract.ActivityResultContracts.RequestMultiplePermissions
 import androidx.core.content.ContextCompat
 import com.g00fy2.developerwidget.base.BaseActivity
@@ -19,8 +26,16 @@ class PermissionControllerImpl @Inject constructor() : PermissionController {
   }
 
   override fun requestPermissions(vararg permissions: String, onGranted: (() -> Unit), onDenied: () -> Unit) {
-    activity.registerForActivityResult(RequestMultiplePermissions()) { result ->
-      if (result.all { it.value }) onGranted() else onDenied()
-    }.launch(permissions)
+    if (VERSION.SDK_INT >= VERSION_CODES.R && permissions.any {
+        it == Manifest.permission.READ_EXTERNAL_STORAGE || it == Manifest.permission.WRITE_EXTERNAL_STORAGE
+      } && !Environment.isExternalStorageManager()) {
+      activity.startActivity(Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION).apply {
+        data = Uri.parse("package:${activity.packageName}")
+      })
+    } else {
+      activity.registerForActivityResult(RequestMultiplePermissions()) { result ->
+        if (result.all { it.value }) onGranted() else onDenied()
+      }.launch(permissions)
+    }
   }
 }
