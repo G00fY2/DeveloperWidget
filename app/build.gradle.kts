e24aaedd01552a07163c9d4c63579515a33c3026
Thomas Wirth <thomas-w-@web.de>
2020-11-04 22:44:38 +0100

    Update kotlin and coroutines

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -79,7 +79,7 @@ repositories {
 }
 dependencies {
   // Kotlin
-  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.4.0")
+  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.4.1")
 
   // AndroidX
   implementation("androidx.appcompat:appcompat:1.3.0-alpha02")
