66000a132e03eb90236070e868bb17b7c4263753
Thomas Wirth <thomas-w-@web.de>
2020-12-01 21:40:17 +0100

    Remove gradle version plugin filter

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,7 @@
-import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
-
 plugins {
   id(Plugins.Android.application) version Versions.androidGradle apply false
   kotlin(Plugins.Kotlin.androidGradle) version Versions.kotlin apply false
   id(Plugins.Misc.gradleVersions) version Versions.gradleVersions
-}
-
-tasks.named("dependencyUpdates", DependencyUpdatesTask::class.java).configure {
-  resolutionStrategy {
-    componentSelection {
-      all {
-        if (Utils.isNonStable(candidate.version) && !Utils.isNonStable(currentVersion)) {
-          reject("Release candidate")
-        }
-      }
-    }
-  }
 }
 
 tasks.register<Delete>("clean") {
