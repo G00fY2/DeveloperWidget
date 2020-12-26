ed98d55fe414b6187b4a45f462fac89a236fc5a5
Thomas Wirth <thomas-w-@web.de>
2020-12-26 18:52:37 +0100

    Update gradle and kotlin

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -2,7 +2,7 @@ object Versions {
 
   const val androidGradle = "7.0.0-alpha03"
   const val buildToolsVersion = "30.0.3"
-  const val kotlin = "1.4.21"
+  const val kotlin = "1.4.30-M1"
 
   const val gradleVersions = "0.36.0"
   const val androidVersioning = "2.4.0"
