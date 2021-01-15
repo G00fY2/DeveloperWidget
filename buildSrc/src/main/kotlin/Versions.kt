78f49dd9d50c328140f65c7df7fc237368a2ed64
Thomas Wirth <thomas-w-@web.de>
2021-01-15 09:33:33 +0100

    Update deps

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -18,10 +18,10 @@ object Versions {
   const val constraintLayout = "2.1.0-alpha2"
   const val vectorDrawable = "1.2.0-alpha02"
 
-  const val materialDesign = "1.3.0-beta01"
+  const val materialDesign = "1.3.0-rc01"
 
   const val timber = "4.7.1"
   const val versionCompare = "1.3.7"
 
-  const val dagger = "2.30.1"
+  const val dagger = "2.31"
 }
