3a7bbece6370d599be1bbad1206e62881d5d7ef8
Thomas Wirth <thomas-w-@web.de>
2021-02-12 00:13:21 +0100

    Update android studio and deps

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,6 +1,6 @@
 object Versions {
 
-  const val androidGradle = "7.0.0-alpha05"
+  const val androidGradle = "7.0.0-alpha06"
   const val buildToolsVersion = "30.0.3"
   const val kotlin = "1.4.30"
 
@@ -11,17 +11,17 @@ object Versions {
 
   const val appcompat = "1.3.0-beta01"
   const val core = "1.5.0-beta01"
-  const val activity = "1.2.0-rc01"
-  const val fragment = "1.3.0-rc02"
-  const val lifecycle = "2.3.0-rc01"
+  const val activity = "1.3.0-alpha02"
+  const val fragment = "1.3.0"
+  const val lifecycle = "2.3.0"
   const val recyclerView = "1.2.0-beta01"
   const val constraintLayout = "2.1.0-alpha2"
   const val vectorDrawable = "1.2.0-alpha02"
 
-  const val materialDesign = "1.3.0-rc01"
+  const val materialDesign = "1.3.0"
 
   const val timber = "4.7.1"
   const val versionCompare = "1.3.7"
 
-  const val dagger = "2.31.2"
+  const val dagger = "2.32"
 }
