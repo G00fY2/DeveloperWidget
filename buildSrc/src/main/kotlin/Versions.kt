26941475a8fcdc6bfb289d5593bd05d4cd8181ac
Thomas Wirth <thomas-w-@web.de>
2021-03-12 09:23:31 +0100

    Update deps

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -10,12 +10,12 @@ object Versions {
   const val coroutines = "1.4.3"
 
   const val appcompat = "1.3.0-beta01"
-  const val core = "1.5.0-beta02"
+  const val core = "1.5.0-beta03"
   const val activity = "1.3.0-alpha04"
   const val fragment = "1.3.1"
   const val lifecycle = "2.3.0"
   const val recyclerView = "1.2.0-beta02"
-  const val constraintLayout = "2.1.0-alpha2"
+  const val constraintLayout = "2.1.0-beta01"
   const val vectorDrawable = "1.2.0-alpha02"
 
   const val materialDesign = "1.4.0-alpha01"
