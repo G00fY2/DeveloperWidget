2a134e41b32502c2c1b3328aefbb49bfdc1b0679
Thomas Wirth <thomas-w-@web.de>
2021-05-05 22:43:17 +0200

    Update dependencies

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -10,12 +10,12 @@ object Versions {
   const val coroutines = "1.5.0-RC"
 
   const val appcompat = "1.3.0-rc01"
-  const val core = "1.6.0-alpha02"
+  const val core = "1.6.0-alpha03"
   const val activity = "1.3.0-alpha07"
   const val fragment = "1.3.3"
   const val lifecycle = "2.4.0-alpha01"
   const val recyclerView = "1.2.0"
-  const val constraintLayout = "2.1.0-beta01"
+  const val constraintLayout = "2.1.0-beta02"
   const val vectorDrawable = "1.2.0-alpha02"
 
   const val materialDesign = "1.4.0-alpha02"
@@ -23,5 +23,5 @@ object Versions {
   const val timber = "4.7.1"
   const val versionCompare = "1.4.1"
 
-  const val dagger = "2.35"
+  const val dagger = "2.35.1"
 }
