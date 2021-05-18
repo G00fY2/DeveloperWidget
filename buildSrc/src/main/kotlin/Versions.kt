229fbf51b8b94d8c93020961b82217903b558774
Thomas Wirth <thomas-w-@web.de>
2021-05-18 22:05:25 +0200

    Update deps, gradle, android studio

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,7 +1,7 @@
 object Versions {
 
-  const val androidGradle = "7.0.0-alpha15"
-  const val buildToolsVersion = "31.0.0-rc3"
+  const val androidGradle = "7.1.0-alpha01"
+  const val buildToolsVersion = "31.0.0-rc4"
   const val kotlin = "1.5.0"
 
   const val gradleVersions = "0.38.0"
@@ -9,10 +9,10 @@ object Versions {
 
   const val coroutines = "1.5.0"
 
-  const val appcompat = "1.3.0-rc01"
-  const val core = "1.6.0-alpha03"
-  const val activity = "1.3.0-alpha07"
-  const val fragment = "1.3.3"
+  const val appcompat = " 1.4.0-alpha01"
+  const val core = "1.6.0-beta01"
+  const val activity = "1.3.0-alpha08"
+  const val fragment = "1.4.0-alpha01"
   const val lifecycle = "2.4.0-alpha01"
   const val recyclerView = "1.2.0"
   const val constraintLayout = "2.1.0-beta02"
