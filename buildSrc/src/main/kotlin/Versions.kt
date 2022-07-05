8f49177abd12f67d782d3bc9f3236dd38672fa70
Thomas Wirth <thomas-w-@web.de>
2022-07-05 23:42:36 +0200

    Update AndroidX dependencies

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -14,9 +14,9 @@ object Versions {
 
   const val appcompat = "1.4.2"
   const val core = "1.8.0"
-  const val activity = "1.4.0"
-  const val fragment = "1.4.1"
-  const val lifecycle = "2.4.1"
+  const val activity = "1.5.0"
+  const val fragment = "1.5.0"
+  const val lifecycle = "2.5.0"
   const val recyclerView = "1.2.1"
   const val constraintLayout = "2.1.4"
   const val vectorDrawable = "1.1.0"
