e62542b4c63700a5f22429552aa29ce45cee84f7
Thomas Wirth <thomas-w-@web.de>
2022-08-01 03:20:23 +0200

    Update Gradle and dependencies

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -6,17 +6,17 @@ object Versions {
 
   const val androidBuildTools = "32.0.0"
   const val androidGradle = "7.2.1"
-  const val kotlin = "1.7.0"
+  const val kotlin = "1.7.10"
 
   const val gradleVersions = "0.42.0"
 
-  const val coroutines = "1.6.3"
+  const val coroutines = "1.6.4"
 
   const val appcompat = "1.4.2"
   const val core = "1.8.0"
-  const val activity = "1.5.0"
-  const val fragment = "1.5.0"
-  const val lifecycle = "2.5.0"
+  const val activity = "1.5.1"
+  const val fragment = "1.5.1"
+  const val lifecycle = "2.5.1"
   const val recyclerView = "1.2.1"
   const val constraintLayout = "2.1.4"
   const val vectorDrawable = "1.1.0"
@@ -26,7 +26,7 @@ object Versions {
   const val timber = "5.0.1"
   const val versionCompare = "1.5.0"
 
-  const val dagger = "2.42"
+  const val dagger = "2.43.1"
 
   private val sortedReleaseQualifiers = listOf("alpha", "beta", "m", "rc")
 
